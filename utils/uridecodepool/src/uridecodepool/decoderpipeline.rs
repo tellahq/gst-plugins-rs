e337ad8ab141610c5b72bef916ca5eaecf5d68db
Thibault Saunier <tsaunier@igalia.com>
2025-04-28 21:46:24 -0400

    uridecodepool: Lower wrong debug logging level

--- utils/uridecodepool/src/uridecodepool/decoderpipeline.rs
+++ utils/uridecodepool/src/uridecodepool/decoderpipeline.rs
@@ -499,7 +499,7 @@ impl DecoderPipeline {
             return false;
         }
 
-        gst::error!(CAT, obj = pipeline, "--> Sending seek {:?}", seek_event);
+        gst::info!(CAT, obj = pipeline, "--> Sending seek {:?}", seek_event);
         if !pipeline.send_event(seek_event) {
             gst::error!(CAT, obj = pipeline, "Failed to seek");
             return false;
