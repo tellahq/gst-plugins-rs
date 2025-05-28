dcb192a44126680adc42d06733f9c3b90fbfc4b6
Thibault Saunier <tsaunier@igalia.com>
2025-05-28 11:35:20 +0200

    uridecodepoolsrc: Minor debug enhancements

--- utils/uridecodepool/src/uridecodepool/decoderpipeline.rs
+++ utils/uridecodepool/src/uridecodepool/decoderpipeline.rs
@@ -478,7 +478,7 @@ impl DecoderPipeline {
 
                         Some(stream)
                     } else {
-                        gst::warning!(
+                        gst::info!(
                             CAT,
                             obj = self.pipeline_ref(),
                             "{:?} requested stream {} not found in {} - available: {:?}",
