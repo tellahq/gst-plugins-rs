cae4ba84c5c4756d5c1f0a95d8cba343e61ffc8c
Michiel Westerbeek <happylinks@gmail.com>
2025-11-14 09:17:35 +0100

    Revert "rsvalidate: Add compare-last-frame action for visual regression testing"
    This reverts commit f57293989c4fc0bd3e30cdd4dc90adaffacff7b7.

--- utils/validate/src/lib.rs
+++ utils/validate/src/lib.rs
@@ -2,22 +2,10 @@
 
 use gst::glib;
 
-mod utils;
 mod check_last_frame_qrcode;
-mod compare_last_frame;
 
 fn plugin_init(plugin: &gst::Plugin) -> Result<(), glib::BoolError> {
     if let Err(err) = check_last_frame_qrcode::register_validate_actions(plugin) {
-        gst::warning!(
-            gst::CAT_RUST,
-            "Failed to register validate actions: {}",
-            err
-        );
-
-        return Err(err);
-    }
-
-    if let Err(err) = compare_last_frame::register_validate_actions(plugin) {
         gst::warning!(
             gst::CAT_RUST,
             "Failed to register validate actions: {}",
